6206828e4b769b92ef9c62f35ce267e6e2eb629d
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-18 13:09:00 -1000

    Practicing some variables in JS.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,7 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is fun!');
 
 let result = 40 + 8 + 23 - 10;
-console.log(result);
+let firstName = "Jesse"
+let num = 23
+
+console.log(result, firstName, num);
