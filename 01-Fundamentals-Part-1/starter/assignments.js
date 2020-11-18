62618b13bbdf2c2f8c3e68b342a304552e012b40
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-18 13:44:11 -1000

    Finished lecture 2. Prepared for Lecture 3. Prepared for Assignment 2.

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,5 +1,12 @@
+/*
+ASSIGNMENT 1
 let country = "United States"
 let continent = "North America"
 let population = 328
 
 console.log(country, continent, population)
+*/
+
+/*
+ASSIGNMENT 2
+*/
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -29,3 +29,30 @@ BigInt: Larger integers than the Number type can hold.
 JavaScript has dynamic typing: We do not have to manually define the data type of the value stored in a var. Instead, data types are determined automatically.
 */
 
+/*
+LECTURE 2 - DATA TYPES
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jesse");
+
+// Dynamic Typing
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+// Undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2020;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null); // Prints out object which is wrong.
+*/
