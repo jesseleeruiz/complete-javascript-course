5028a3d81828af0781eab45436f81561c9b49632
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-18 13:33:05 -1000

    Added some notes. Prepared for lecture 2.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,5 @@
+/*
+LECTURE 1
 let js = 'amazing';
 
 let result = 40 + 8 + 23 - 10;
@@ -5,3 +7,25 @@ let firstName = "Jesse"
 let num = 23
 
 console.log(result, firstName, num);
+
+NOTES
+Number: Floating point numbers -> Used for decimals & integers.
+let age = 23;
+
+String: Sequence of characters -> Used for text.
+let firstName = "Jesse";
+
+Boolean: Logical type that can only be true or false.
+
+Undefined: Value taken by a var that is not yet defined ('empty value').
+let children;
+
+Null: Also means 'empty value'.
+
+Symbol: Value that is unique and cannot be changed.
+
+BigInt: Larger integers than the Number type can hold.
+
+JavaScript has dynamic typing: We do not have to manually define the data type of the value stored in a var. Instead, data types are determined automatically.
+*/
+
