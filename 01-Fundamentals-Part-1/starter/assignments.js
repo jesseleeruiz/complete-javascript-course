bd44e0d3d2c0ac9f0cbd483694d1f696880dc5cf
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-18 14:17:33 -1000

    Completed Assignment 2.

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -9,4 +9,16 @@ console.log(country, continent, population)
 
 /*
 ASSIGNMENT 2
+
+let isIsland = true;
+let language;
+
+let country = "United States";
+let continent = "North America";
+let population = 328;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
 */
